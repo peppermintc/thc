e85d30dba7152f5ec8aa71a11fb4aac6f7bc5318
Hamin <haminjp@naver.com>
2021-01-02 14:01:08 +0900

    fix: cateogySlider controll fix

--- src/components/category_slider/category_slider.jsx
+++ src/components/category_slider/category_slider.jsx
@@ -1,9 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./category_slider.module.css";
-import Slider from "react-slick";
 import CategorySliderItem from "../category_slider_item/category_slider_item";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import c1 from "../../images/c1.png";
 import c2 from "../../images/c2.png";
 import c3 from "../../images/c3.png";
@@ -20,54 +17,29 @@ import c13 from "../../images/c13.png";
 import c14 from "../../images/c14.png";
 import c15 from "../../images/c15.png";
 
-function SampleNextArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{
-        ...style,
-        display: "block",
-        background: "none",
-        right: "-10px",
-      }}
-      onClick={onClick}
-    />
-  );
-}
-
-function SamplePrevArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{
-        ...style,
-        display: "block",
-        background: "none",
-        left: "-25px",
-      }}
-      onClick={onClick}
-    />
-  );
-}
-
 const CategorySlider = (props) => {
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 10,
-    slidesToScroll: 5,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
+  const myRef = useRef();
+  const prevClick = () => {
+    const slide = myRef.current;
+    slide.scrollLeft -= slide.offsetWidth;
+    if (slide.scrollLeft <= 0) {
+      slide.scrollLeft = slide.scrollWidth;
+    }
+  };
+
+  const nextClick = () => {
+    const slide = myRef.current;
+    slide.scrollLeft += slide.offsetWidth;
+    if (slide.scrollLeft >= slide.scrollWidth - slide.offsetWidth) {
+      slide.scrollLeft = 0;
+    }
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.inner}>
         <div className={styles.title}>카테코리별 상품 찾기</div>
-        <Slider {...settings}>
+        <div className={styles.slider} ref={myRef}>
           <CategorySliderItem description={"가전"} img={c1} />
           <CategorySliderItem description={"가구"} img={c2} />
           <CategorySliderItem description={"패브릭"} img={c3} />
@@ -83,7 +55,40 @@ const CategorySlider = (props) => {
           <CategorySliderItem description={"디자인문구"} img={c13} />
           <CategorySliderItem description={"수납가구"} img={c14} />
           <CategorySliderItem description={"욕실용품"} img={c15} />
-        </Slider>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.prev} onClick={prevClick}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="white"
+              classnames="bi bi-chevron-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+              />
+            </svg>
+          </div>
+          <div className={styles.next} onClick={nextClick}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="white"
+              classnames="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </div>
+        </div>
       </div>
     </div>
   );
