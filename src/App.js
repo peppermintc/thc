e3b0f7a381a2abc8a4e4c7ca82cac683ec5d5cf9
Hamin <haminjp@naver.com>
2021-01-03 00:24:02 +0900

    feat: banner responsive

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
     <div className={styles.App}>
       <Header />
       <MenuBar />
-      {/* <Banner />
-      <Shortcuts />
+      <Banner />
+      {/* <Shortcuts />
       <Story />
       <CategorySlider />
       <Deal />
